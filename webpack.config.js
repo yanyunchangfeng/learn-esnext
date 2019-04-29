e70fd190f48718547115d035f832e5e20d552a54
燕云长风 <yanyunchangfeng@gmail.com>
2019-04-29 11:07:15 +0800

    init webpack config

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports={
+    entry: path.join(__dirname,'app/js/index.js'),
+    mode:'development',
+    output:{
+        path:path.resolve(__dirname,'dist'),
+        filename:'[name].bundle.js'
+    },
+    resolve:{
+        extensions:['*','.js']
+    },
+    module:{
+        rules:[
+            {
+                test:/\.js/,
+                exclude:/node_modules/,
+                use:['babel-loader']
+            }
+        ]
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, "app/index.temp.html"),
+            filename: "index.html",
+            favicon:path.join(__dirname,'app/img/yanyunchangfeng.png')
+          }),
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer:{
+        contentBase:'./dist'
+    }
+}
