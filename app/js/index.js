02a715869c9e25d1bfd5e06adaf16b4f85b74396
燕云长风 <yanyunchangfeng@gmail.com>
2019-04-30 11:17:01 +0800

    add arr extension

--- app/js/index.js
+++ app/js/index.js
@@ -2,4 +2,5 @@
 // import './lesson/lesson2';      
 // import './lesson/lesson3';      
 // import './lesson/lesson4';      
-import './lesson/lesson5';      
+// import './lesson/lesson5';      
+import './lesson/lesson6';      
