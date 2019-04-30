b3a5eb50f2cb4607af3530376a8b8baad15bb0a9
燕云长风 <yanyunchangfeng@gmail.com>
2019-04-30 10:20:32 +0800

    add Number extension

--- app/js/index.js
+++ app/js/index.js
@@ -1,4 +1,5 @@
 // import './lesson/lesson1';
 // import './lesson/lesson2';      
-import './lesson/lesson3';      
+// import './lesson/lesson3';      
 // import './lesson/lesson4';      
+import './lesson/lesson5';      
