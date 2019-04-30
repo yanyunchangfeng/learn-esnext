305a3e9baf5351beb72fa04d354a02a96d963042
燕云长风 <yanyunchangfeng@gmail.com>
2019-04-30 15:34:44 +0800

    add Symbol extension

--- app/js/index.js
+++ app/js/index.js
@@ -5,4 +5,5 @@
 // import './lesson/lesson5';      
 // import './lesson/lesson6';      
 // import './lesson/lesson7';      
-import './lesson/lesson8';      
+// import './lesson/lesson8';      
+import './lesson/lesson9';      
